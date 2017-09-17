873f1e32244568ef47df0f0025002d1d4b8da7c0
chaoyang.gcy <chaoyang.gcy@alibaba-inc.com>
2017-09-17 19:14:15 +0800

    push

--- CaibeiMarket/AppDelegate.m
+++ CaibeiMarket/AppDelegate.m
@@ -10,7 +10,9 @@
 #import "XGManager.h"
 #import "MTAManager.h"
 #import "BuglyManager.h"
+#import "XGPush.h"
 #import <UserNotifications/UserNotifications.h>
+#import <UserNotifications/UNUserNotificationCenter.h>
 
 @interface AppDelegate () <UISplitViewControllerDelegate,UNUserNotificationCenterDelegate>
 
@@ -30,6 +32,7 @@
     [BuglyManager initBugly];
     [MTAManager initMTA];
     [MTAManager trackCustomEvent:@"icaibei_start_ios" args:@[@""]];
+    [self registerAPNS];
 
     [self initWindow];
     
@@ -56,21 +59,6 @@
     
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-    [XGManager registerAPNSToken:deviceToken];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-{
-    [XGManager handleReceiveNotification:userInfo];
-}
-
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
-{
-
-}
-
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -92,10 +80,217 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler
-{
-    [XGManager handleLaunching:response.notification.request.content.userInfo];
-    completionHandler();
+#pragma mark - Handle url scheme
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    //    return [UMSocialSnsService handleOpenURL:url];
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    if ([url.scheme isEqualToString:@"BDSprite"]) {
+//        [self captureUrlScheme:url];
+        return YES;
+    }
+    
+    return YES;
+}
+
+//- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+//didReceiveNotificationResponse:(UNNotificationResponse *)response
+//         withCompletionHandler:(void(^)())completionHandler
+//{
+//    [XGManager handleReceiveNotification:response.notification];
+//}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    //    NSString * str = [[NSString alloc] initWithData:deviceToken encoding:NSUTF8StringEncoding];
+    
+    NSString *deviceTokenStr = [XGPush registerDevice:deviceToken account:nil successCallback:^{
+        NSLog(@"[XGDemo] register push success");
+//        [SVProgressHUD showSuccessWithStatus:@"token regist success!!"];
+    } errorCallback:^{
+        NSLog(@"[XGDemo] register push error");
+    }];
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:deviceTokenStr forKey:@"BS_Device_Token"];
+    [userDefaults synchronize];
+    //保存 device token 令牌,并且去掉空格
+    NSLog(@"[XGDemo] device token is %@", deviceTokenStr);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"[XGDemo] register APNS fail.\n[XGDemo] reason : %@", error);
+}
+
+/**
+ 收到通知的回调
+ 
+ @param application  UIApplication 实例
+ @param userInfo 推送时指定的参数
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"[XGDemo] receive Notification");
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:userInfo.description forKey:@"aa"];
+    [userDefaults synchronize];
+    
+//    [self handlerNotification:userInfo];
+    
+        NSString * message = [NSString stringWithFormat:@"%@",userInfo.description];
+        [XGPush handleReceiveNotification:userInfo
+                          successCallback:^{
+                              NSLog(@"[XGDemo] Handle receive success");
+                          } errorCallback:^{
+                              NSLog(@"[XGDemo] Handle receive error");
+                          }];
+}
+
+
+/**
+ 收到静默推送的回调
+ 
+ @param application  UIApplication 实例
+ @param userInfo 推送时指定的参数
+ @param completionHandler 完成回调
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    NSLog(@"[XGDemo] receive slient Notification");
+    NSLog(@"[XGDemo] userinfo %@", userInfo);
+    //    [XGPush handleReceiveNotification:userInfo
+    //                      successCallback:^{
+    //                          NSLog(@"[XGDemo] Handle receive success");
+    //                      } errorCallback:^{
+    //                          NSLog(@"[XGDemo] Handle receive error");
+    //                      }];
+    
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+// iOS 10 新增 API
+// iOS 10 会走新 API, iOS 10 以前会走到老 API
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// App 用户点击通知的回调
+// 无论本地推送还是远程推送都会走这个回调
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler {
+//    NSDictionary * userInfo = [response.notification.request.content userInfo];
+    NSLog(@"[XGDemo] click notification");
+    
+    NSArray *localNotifications = [UIApplication sharedApplication].scheduledLocalNotifications;
+    
+    
+    [[UNUserNotificationCenter currentNotificationCenter] getPendingNotificationRequestsWithCompletionHandler:^(NSArray<UNNotificationRequest *> * _Nonnull requests) {
+        NSArray * array = requests;
+        NSLog(@"%@",array);
+    }];
+    
+    
+    [[UNUserNotificationCenter currentNotificationCenter] getDeliveredNotificationsWithCompletionHandler:^(NSArray<UNNotification *> * _Nonnull notifications) {
+        NSArray * array = notifications;
+        NSLog(@"%@",array);
+    }];
+    
+    for (UILocalNotification *notification in localNotifications)
+    {
+        NSDictionary *userInfo = notification.userInfo;
+        if (userInfo)
+        {
+            // 根据设置通知参数时指定的key来获取通知参数
+            NSString *info = userInfo[@"aaa"];
+            
+            // 如果找到需要取消的通知，则取消
+            if (info != nil)
+            {
+                [[UIApplication sharedApplication] cancelLocalNotification:notification];
+                break;
+            }
+        }
+    }
+    
+//    [self handlerNotification:userInfo];
+    
+    //    [XGPush handleReceiveNotification:response.notification.request.content.userInfo
+    //                      successCallback:^{
+    //                          NSLog(@"[XGDemo] Handle receive success");
+    //                      } errorCallback:^{
+    //                          NSLog(@"[XGDemo] Handle receive error");
+    //                      }];
+    
+    //    completionHandler();
+}
+
+// App 在前台弹通知需要调用这个接口
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler {
+    NSLog(@"aa");
+    //    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+}
+#endif
+
+- (void)registerAPNS {
+    float sysVer = [[[UIDevice currentDevice] systemVersion] floatValue];
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    if (sysVer >= 10) {
+        // iOS 10
+        [self registerPush10];
+    } else if (sysVer >= 8) {
+        // iOS 8-9
+        [self registerPush8to9];
+    } else {
+        // before iOS 8
+        [self registerPushBefore8];
+    }
+#else
+    if (sysVer < 8) {
+        // before iOS 8
+        [self registerPushBefore8];
+    } else {
+        // iOS 8-9
+        [self registerPush8to9];
+    }
+#endif
+}
+
+- (void)registerPush10{
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+    
+    [center requestAuthorizationWithOptions:UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+        }
+    }];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+#endif
+}
+
+- (void)registerPush8to9{
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+
+- (void)registerPushBefore8{
+    //#ifdef __IPHONE_8_0
+    //    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+    //#endif
+}
+
+#pragma mark - Status bar touch tracking
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    CGPoint location = [[[event allTouches] anyObject] locationInView:[self window]];
+    CGRect statusBarFrame = [UIApplication sharedApplication].statusBarFrame;
+    if (CGRectContainsPoint(statusBarFrame, location)) {
+        [self statusBarTouchedAction];
+    }
+}
+
+- (void)statusBarTouchedAction {
+    [[NSNotificationCenter defaultCenter] postNotificationName:DidTapStatusBar object:nil];
 }
 
 @end
